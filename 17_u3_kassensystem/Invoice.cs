7e8223ccb7997c1272a829b5ca3704bf5b850abe
David Schuler <d.schuler@kbbz-halberg.de>
2021-02-22 12:16:42 +0100

    Start Übung 17-3 - Lösung von Übungsblatt 14 kopiert

---
+++ 17_u3_kassensystem/Invoice.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _17_u3_kassensystem
+{
+
+  public class Product
+  {
+    public int ID { get; }
+
+    public string Name { get; set; }
+
+    public string Desc { get; set; }
+
+    public decimal Price { get; set; }
+
+    public Product(int id)
+    {
+      ID = id;
+    }
+
+    public override string ToString()
+    {
+      return $"{ID} - {Name} - {Desc} - {Price}";
+    }
+  }
+
+
+  public class InvoiceItem
+  {
+    public Product Product { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal Total
+    {
+      get
+      {
+        return Product.Price * Quantity;
+      }
+      set { }
+    }
+
+    public InvoiceItem(Product product, int quantity)
+    {
+      Product = product;
+      Quantity = quantity;
+    }
+
+    public override string ToString()
+    {
+      return $"{Quantity} x {Product.Name} Preis: {Total} ";
+    }
+  }
+
+  public class Address
+  {
+    public string Street { get; set; }
+    public string City { get; set; }
+
+  }
+
+  public class Customer
+  {
+    public string Name { get; set; }
+
+    public Address Address { get; set; }
+  }
+
+
+
+  public class Invoice
+  {
+    public int InvoiceID { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public Customer Customer { get; set; }
+
+    public List<InvoiceItem> Items { get; set; } = new List<InvoiceItem>();
+
+    public decimal Total
+    {
+      get
+      {
+        return Items.Sum(x => x.Total);
+      }
+      set { }
+    }
+
+
+    public static Invoice GetExampleInvoice()
+    {
+      Address a = new Address()
+      {
+        Street = "Mainzer Str. 123",
+        City = "Saarbrücken"
+      };
+
+      Customer c = new Customer()
+      {
+        Name = "Alice",
+        Address = a
+      };
+
+      Product bananas = new Product(1)
+      {
+        Name = "Bananen",
+        Desc = "Bananen aus Brasilien (1 kg)",
+        Price = 1.99m
+      };
+
+
+      Product apples = new Product(2)
+      {
+        Name = "Äpfel",
+        Desc = "Äpfel aus Österreich (1 kg)",
+        Price = 2.99m
+      };
+      Product potatoes = new Product(3)
+      {
+        Name = "Kartoffeln",
+        Desc = "Kartoffeln aus der Pfalz (1kg)",
+        Price = 1.49m
+      };
+      InvoiceItem i1 = new InvoiceItem(bananas, 4);
+      InvoiceItem i2 = new InvoiceItem(apples, 2);
+      InvoiceItem i3 = new InvoiceItem(potatoes, 4);
+
+      Invoice ExampleInvoice = new Invoice()
+      {
+        InvoiceID = 111231223,
+        Customer = c,
+        Date = DateTime.Now,
+      };
+      ExampleInvoice.Items.Add(i1);
+      ExampleInvoice.Items.Add(i2);
+      ExampleInvoice.Items.Add(i3);
+      return ExampleInvoice;
+    }
+
+
+  }
+
+}
---
+++ 17_u3_kassensystem/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace _17_u3_kassensystem
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+
+    private static Invoice Cart = new Invoice();
+
+
+    public List<Product> Stock = new List<Product>();
+
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      Product bananas = new Product(1)
+      {
+        Name = "Bananen",
+        Desc = "Bananen aus Brasilien (1 kg)",
+        Price = 1.99m
+      };
+      Product apples = new Product(2)
+      {
+        Name = "Äpfel",
+        Desc = "Äpfel aus Österreich (1 kg)",
+        Price = 2.99m
+      };
+      Product potatoes = new Product(3)
+      {
+        Name = "Kartoffeln",
+        Desc = "Kartoffeln aus der Pfalz (1kg)",
+        Price = 1.49m
+      };
+      Product cheese = new Product(4)
+      {
+        Name = "Käse",
+        Desc = "Käse aus der Schweiz (1kg)",
+        Price = 9.49m
+      };
+
+      Stock.Add(bananas);
+      Stock.Add(cheese);
+      Stock.Add(potatoes);
+
+      Sortiment.DataContext = Stock;
+
+      Warenkorb.ItemsSource = Cart.Items;
+
+    }
+
+
+
+    private void Sortiment_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+      bool present = false;
+      var selected = Sortiment.SelectedItem;
+      if (selected != null)
+      {
+        Product selectedProduct = (Product)selected;
+        foreach (InvoiceItem i in Cart.Items)
+        {
+          if (selectedProduct == i.Product)
+          {
+            i.Quantity++;
+            present = true;
+            Warenkorb_SourceUpdated();
+          }
+        }
+        if (!present)
+        {
+          InvoiceItem newi = new InvoiceItem(selectedProduct, 1);
+          Cart.Items.Add(newi);
+          Warenkorb_SourceUpdated();
+        }
+      }
+    }
+
+
+    private void Warenkorb_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+      if (Cart.Items[Warenkorb.SelectedIndex].Quantity == 1)
+      {
+        Cart.Items.RemoveAt(Warenkorb.SelectedIndex);
+      }
+      else
+      {
+        Cart.Items[Warenkorb.SelectedIndex].Quantity--;
+      }
+      Warenkorb_SourceUpdated();
+    }
+
+    private void Neuberechnung_Click(object sender, RoutedEventArgs e)
+    {
+      Binding TotalBinding = new Binding("")
+      {
+        Source = Cart.Total
+      };
+      FinalTotal.SetBinding(Label.ContentProperty, TotalBinding);
+    }
+
+    private void Warenkorb_SourceUpdated()
+    {
+      Warenkorb.ItemsSource = null;
+      Warenkorb.ItemsSource = Cart.Items;
+    }
+  }
+}
