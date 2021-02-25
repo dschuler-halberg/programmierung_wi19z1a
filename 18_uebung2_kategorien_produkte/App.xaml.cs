aa30b488fe679812db8a55f96dc3a43db026a796
David Schuler <d.schuler@kbbz-halberg.de>
2021-02-25 10:42:01 +0100

    Lösung Übung 18.2

---
+++ 18_uebung2_kategorien_produkte/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _18_uebung2_kategorien_produkte
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
+}
---
+++ 18_uebung2_kategorien_produkte/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _18_uebung2_kategorien_produkte
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      AdventureWorksContext ctx = new AdventureWorksContext();
+      ctx.ProductCategory.Load();
+      ParentGrid.DataContext = ctx.ProductCategory.Local;
+    }
+
+  }
+}
