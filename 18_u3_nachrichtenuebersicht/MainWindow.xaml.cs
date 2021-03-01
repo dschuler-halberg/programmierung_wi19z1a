ca6146a4cce407f9a74940f51f1564c4426a7f76
David Schuler <d.schuler@kbbz-halberg.de>
2021-03-01 10:49:45 +0100

    Zwischenstand Übung 18.3

--- 18_u3_nachrichtenuebersicht/MainWindow.xaml.cs
+++ 18_u3_nachrichtenuebersicht/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using bsp_ef_wpf;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +21,58 @@ namespace _18_u3_nachrichtenuebersicht
   /// <summary>
   /// Interaction logic for MainWindow.xaml
   /// </summary>
+
+
   public partial class MainWindow : Window
   {
+    private ICollectionView CollectionView;
+
+    private MessengerContext Context = new MessengerContext();
+
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      Context.Nachrichten.Load();
+      CollectionView = CollectionViewSource.GetDefaultView(Context.Nachrichten.Local);
+      ParentGrid.DataContext = CollectionView;
+    }
+
+
     public MainWindow()
     {
       InitializeComponent();
+
+
+    //  Nachricht current = (Nachricht)CollectionView.CurrentItem;
+    //  List<Nachricht> nachrichten = Context.Nachrichten.ToList();
+    //  int indexOfCurrent = nachrichten.IndexOf(current);
+    //  string searchString = "";
+    ////  List<Nachricht> searchResults = Context.Nachrichten.Where(x => x.Text.Contains(searchString)).ToList();
+    //  List<Nachricht> searchResults = Context.Nachrichten.Skip(indexOfCurrent).Where(x => x.Text.Contains(searchString)).ToList();
+    //  //CollectionView.MoveCurrentTo();
+    }
+
+    private void weiter_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void suche_KeyUp(object sender, KeyEventArgs e)
+    {
+      if (e.Key == Key.Enter)
+      {
+
+
+      }
+      else
+      {
+        string searchString = suche.Text;
+        List<Nachricht> searchResults = Context.Nachrichten.Where(x => x.Text.Contains(searchString)).ToList();
+        if (searchResults.Count > 0)
+        {
+          CollectionView.MoveCurrentTo(searchResults[0]);
+        }
+      }
     }
   }
 }
